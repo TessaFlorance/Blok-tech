b1e97de04541cef7e9dd60a6938a7b732de20d5d
Tessa Welling <tessafwelling@gmail.com>
2022-03-07 13:02:11 +0100

    profielen
    nieuwe profielen

--- server.js
+++ server.js
@@ -8,25 +8,33 @@ const port = 3000
 const profielen = [
     {
         "naam": "Sophie",
-        "geboortedatum": "27-01-2001",
-        "hobbie": "knutselen",
-        "duur": 2,
+        "geboortedatum": "27-01-2000",
+        "hobbie": "Klimmen",
+        "duur": "2 weken",
         "beschrijving": "dit wordt nog een keer echt een mooie lange beschrijving"
     }, 
 
     {
-        "naam": "Sophie",
-        "geboortedatum": "27-01-2001",
-        "hobbie": "knutselen",
-        "duur": 2,
+        "naam": "Jesper",
+        "geboortedatum": "02-04-1999",
+        "hobbie": "Klimmen",
+        "duur": "2 weken" ,
         "beschrijving": "dit wordt nog een keer echt een mooie lange beschrijving"
     }, 
 
     {
-        "naam": "Sophie",
-        "geboortedatum": "27-01-2001",
-        "hobbie": "knutselen",
-        "duur": 2,
+        "naam": "Rosalie",
+        "geboortedatum": "14-07-1995",
+        "hobbie": "Klimmen",
+        "duur": "2 weken",
+        "beschrijving": "dit wordt nog een keer echt een mooie lange beschrijving"
+    },
+
+    {
+        "naam": "Kevin",
+        "geboortedatum": "23-02-1998",
+        "hobbie": "Klimmen",
+        "duur": "2 weken",
         "beschrijving": "dit wordt nog een keer echt een mooie lange beschrijving"
     }
 ]
@@ -43,6 +51,7 @@ app.set('view engine', 'ejs')
 
 //om url te linken app.get('/aanmelden)
 //{aanmelden: aanmeld pagina } is ejs tekst template kan ook zonder 
+//'aanmelden' is de ejs/html pagina
 app.get('/aanmelden', (req, res) => {
     res.render('aanmelden', {aanmelden: 'aanmeld pagina'})
 })
@@ -50,11 +59,17 @@ app.get('/aanmelden', (req, res) => {
 app.get('/profielen', (req, res) => {
     res.render('profielen',{profielen} )
 })
+ 
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
-app.get('/', (req, res) => {})
+app.get('/registeren', (req, res) => {
+    res.render('registeren')
+})
+
 //listen on port 3000
 app.listen(port, () => console.info(`listening on port ${port}`))
-
 
 //error 
 app.use( (req, res) => {
